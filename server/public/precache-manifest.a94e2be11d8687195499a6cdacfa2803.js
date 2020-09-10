90fbea0497c4c64375fad4cd868b462cf9640f52
aless-ishy <alessandro.ishy.medeiros@gmail.com>
2020-09-10 11:40:18 -0400

    update gitignore

---
+++ server/public/precache-manifest.a94e2be11d8687195499a6cdacfa2803.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e83dee90a0b4dc77cceb15c7e6012dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f34626f295427294d88e",
+    "url": "/static/css/main.632410d0.chunk.css"
+  },
+  {
+    "revision": "9560335eb983a68c2ee1",
+    "url": "/static/js/2.1e52b873.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1e52b873.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f34626f295427294d88e",
+    "url": "/static/js/main.175ab3a8.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "087dfc07a25a56cbe9287f42b0d55660",
+    "url": "/static/media/switch.087dfc07.svg"
+  }
+]);
